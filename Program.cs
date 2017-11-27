5962d58fe60d00e6da519bbcc56c1aea91b33670
Yonatan Higgsmith <yoni.higgsmith@gmail.com>
2017-11-27 18:16:20 +0000

    Animate the console title and options for changing the window size

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+           
+        // can make console window shrink or grow with this.
+                //for (int i = 52; i > 40; i--)
+                //{
+                //    Console.SetWindowSize(i, i);
+                //    System.Threading.Thread.Sleep(50);
+                //}
+
+            string Progresbar = "SuperCool83 animated title to the console.            ";
+            var title = "";
+            while (true)
+            {
+                for (int i = 0; i < Progresbar.Length; i++)
+                {
+                    title += Progresbar[i];
+                    Console.Title = title;
+                    Thread.Sleep(100);
+                }
+                title = "";
+            }
+
+             
+
+            
+        }
+    }
+}
